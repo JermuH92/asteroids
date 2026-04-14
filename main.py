f87427fef08eafe19cdbf2c4a150b5a097c3329d
JermuH92 <jtkukkohovi@hotmail.com>
2026-04-14 19:55:38 +0300

    Setup CircleShape class

--- main.py
+++ main.py
@@ -3,11 +3,19 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
 def main():
+    # Notify game starting to console with pygame version and game screen properties
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
 
+    # Initialize pygame 
     pygame.init()
 
+    # Global settings
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    # GAME LOOP START
+    
     while True:
 
         log_state()
@@ -18,6 +26,10 @@ def main():
     
         screen.fill('black') 
         pygame.display.flip()
+
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
