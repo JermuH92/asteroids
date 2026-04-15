0330edbd72cf640341eb2b5f3a5432b9077a1ee6
JermuH92 <jtkukkohovi@hotmail.com>
2026-04-15 09:35:08 +0300

    Implement cooldown between shots

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame # type: ignore
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED
+from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, PLAYER_SHOOT_COOLDOWN_SECONDS
 from circleshape import CircleShape
 from shot import Shot
 
@@ -7,6 +7,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+        self.shot_cooldown = 0
     
     # in the Player class
     def triangle(self):
@@ -31,13 +32,19 @@ class Player(CircleShape):
         self.position += rotated_with_speed_vector
     
     def shoot(self):
+        if self.shot_cooldown > 0:
+            return
+        
+        self.shot_cooldown = PLAYER_SHOOT_COOLDOWN_SECONDS
         shot_from_player_pos = Shot(self.position.x, self.position.y)
         shot_vector = pygame.Vector2(0, 1)
         rotated_shot_vector = shot_vector.rotate(self.rotation)
         multiply_with_speed = rotated_shot_vector * PLAYER_SHOOT_SPEED
         shot_from_player_pos.velocity = multiply_with_speed
+
     
     def update(self, dt):
+        self.shot_cooldown -= dt
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w]:
@@ -48,6 +55,5 @@ class Player(CircleShape):
             self.move(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-        
         if keys[pygame.K_SPACE]:
             self.shoot()
