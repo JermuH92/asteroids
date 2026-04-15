3239b58ac7f455006b2317e306f316d27ce8f35f
JermuH92 <jtkukkohovi@hotmail.com>
2026-04-15 10:12:16 +0300

    Add mechanic: larger and medium asteroids split when shot.

--- asteroid.py
+++ asteroid.py
@@ -1,13 +1,32 @@
 import pygame # type: ignore
+import random 
+from logger import log_event
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        angles = random.uniform(20, 50)
+        first_vel = self.velocity.rotate(angles)
+        second_vel = self.velocity.rotate(-angles)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+        first_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        second_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+        first_asteroid.velocity = first_vel * 1.2
+        second_asteroid.velocity = second_vel * 1.2
     
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
 
     def update(self, dt):
         self.position += (self.velocity * dt)
+    
